fd450af58801fe166d2e260da66faf497bdcd900
Jose Eduardo Paes <j.eduardopaes@gmail.com>
2018-11-30 23:09:02 -0200

    Simple hello server

---
+++ server.py
@@ -0,0 +1,22 @@
+
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+
+class MyHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        # First, send a 200 OK response.
+        self.send_response(200)
+
+        # Then send headers.
+        self.send_header('Content-type', 'text/plain; charset=utf-8')
+        self.end_headers()
+
+        # Now, write the response body.
+        self.wfile.write("Hello, HTTP!\n".encode())
+
+if __name__ == '__main__':
+    server_address = ('', 7001)  # Serve on all addresses, port 7001.
+    httpd = HTTPServer(server_address, MyHandler)
+    print("Server rodando na porta: "+str(7001))
+    httpd.serve_forever()
+    
